501a2f13c4af4360372c060396e098f980c1aed5
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-01-18 23:00:28 +0530

    Update WordCountText.java

--- WordCountText.java
+++ WordCountText.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class WordCount {
+public class WordCountText {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int count = 0;
